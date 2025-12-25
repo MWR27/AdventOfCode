7576d2eb7a1f67ca1124666e0dfe80a838c07745
MWR27 <64335495+MWR27@users.noreply.github.com>
2025-12-25 16:34:51 -0500

    Add 2025 Day 4 Part 2 solution

--- 2025/Day4/Day4.java
+++ 2025/Day4/Day4.java
@@ -8,6 +8,7 @@ public class Day4 {
     public static void main(String[] args) throws IOException {
         char[][] grid = to2DArray(Paths.get(args[0]));
         puzzle1(grid);
+        puzzle2(grid);
     }
 
     public static void puzzle1(char[][] grid) {
@@ -30,8 +31,38 @@ public class Day4 {
         System.out.println("Puzzle 1 answer: " + total);
     }
 
+    public static void puzzle2(char[][] grid) {
+        int total = 0;
+        int nRemoved;
+        while ((nRemoved = removeRolls(grid)) > 0) {
+            total += nRemoved;
+        }
+        System.out.println("Puzzle 2 answer: " + total);
+    }
+
     private static int isRoll(int row, int col, char[][] grid) {
         return (row >= 0 && row < grid.length && col >= 0 && col < grid[0].length && grid[row][col] == '@') ? 1 : 0;
+    }
+
+    private static int removeRolls(char[][] grid) {
+        int total = 0;
+
+        for (int row = 0; row < grid.length; row++) {
+            int leftRollCount = 0;
+            int middleRollCount = 0;
+            int rightRollCount = isRoll(row - 1, 0, grid) + isRoll(row, 0, grid) + isRoll(row + 1, 0, grid);
+            for (int col = 0; col < grid[0].length; col++) {
+                leftRollCount = middleRollCount;
+                middleRollCount = rightRollCount;
+                rightRollCount = isRoll(row - 1, col + 1, grid) + isRoll(row, col + 1, grid) + isRoll(row + 1, col + 1, grid);
+                if (grid[row][col] == '@' && leftRollCount + middleRollCount + rightRollCount - 1 < 4) {
+                    grid[row][col] = 'x';
+                    total++;
+                }
+            }
+        }
+
+        return total;
     }
 
     public static char[][] to2DArray(Path path) throws IOException {
